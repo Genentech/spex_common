17577ab586aa16c1fa0e07c53529b15dc00f9d27
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-01-19 16:35:01 +0700

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -f ./common/Dockerfile -t spex.common:latest .
+docker tag spex.common:latest ghcr.io/genentech/spex.common:latest
+docker push ghcr.io/genentech/spex.common:latest
